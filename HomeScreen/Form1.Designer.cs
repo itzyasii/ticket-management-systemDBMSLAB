43144dd3e8388a710ec71fe9875ec468f3e53917
itzyasii <yasirpechuho1@gmail.com>
2023-05-28 17:56:19 +0500

    Started Developing homeScreen along with Sign in and sign up.

--- HomeScreen/Form1.Designer.cs
+++ HomeScreen/Form1.Designer.cs
@@ -28,57 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.HomeButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // HomeButton
             // 
-            this.button1.Location = new System.Drawing.Point(455, 232);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(272, 41);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Press Button";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.HomeButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeButton.Image")));
+            this.HomeButton.Location = new System.Drawing.Point(541, 11);
+            this.HomeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.HomeButton.Name = "HomeButton";
+            this.HomeButton.Size = new System.Drawing.Size(214, 74);
+            this.HomeButton.TabIndex = 0;
+            this.HomeButton.UseVisualStyleBackColor = true;
+            this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
             // 
-            // textBox1
+            // pictureBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(455, 136);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(272, 22);
-            this.textBox1.TabIndex = 1;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(248, 136);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 17);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Enter your name";
+            this.pictureBox1.Location = new System.Drawing.Point(194, 175);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(965, 388);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1236, 586);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1384, 801);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.HomeButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bookme.pk - Home";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button HomeButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- HomeScreen/Form1.cs
+++ HomeScreen/Form1.cs
@@ -12,18 +12,78 @@ namespace HomeScreen
 {
     public partial class Form1 : Form
     {
-        string name;
+
+      
         public Form1()
         {
             InitializeComponent();
+            this.Size = new Size(1400, 840);
+
+
+            targetX = pictureBox1.Left + 100; // Example: slide the image 100 pixels to the right
+            stepSize = 5; // Example: move the image 5 pixels in each step
+
+            timer1.Tick += timer1_Tick; // Hook up the event handler for the Timer control
+            timer1.Start(); // Start the animation
+
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            name = textBox1.Text;
+            isImageChangePending = true;
 
-            MessageBox.Show($"Hello there {name}");
-            textBox1.Text = "";
+        }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private int currentImageIndex = 0;
+        private int targetX;
+        private int stepSize = 5;
+        private bool isAnimating = false;
+
+        private Image[] images = new Image[]
+{
+    Image.FromFile("E:\\Visual Studio Workspace C#\\DBMSLab-project\\ticket-management-systemDBMSLAB\\HomeScreen\\Bus image.jpg"), // Replace with the path to your first image
+    Image.FromFile("E:\\Visual Studio Workspace C#\\DBMSLab-project\\ticket-management-systemDBMSLAB\\HomeScreen\\Busimage2.jpg"), // Replace with the path to your first image
+    Image.FromFile("E:\\Visual Studio Workspace C#\\DBMSLab-project\\ticket-management-systemDBMSLAB\\HomeScreen\\railwayweb.jpg"), // Replace with the path to your first image
+    Image.FromFile("E:\\Visual Studio Workspace C#\\DBMSLab-project\\ticket-management-systemDBMSLAB\\HomeScreen\\rentcarweb.png"), // Replace with the path to your first image
+    // Replace with the path to your second image
+    // Add more images as needed
+};
+    }
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+        if (isAnimating)
+        {
+            if (pictureBox1.Left < targetX)
+            {
+                pictureBox1.Left += stepSize;
+            }
+            else
+            {
+                isAnimating = false;
+                timer1.Stop();
+                pictureBox1.Image = images[currentImageIndex];
+                currentImageIndex = (currentImageIndex + 1) % images.Length;
+                targetX = pictureBox1.Left + 100; // Example: slide the image 100 pixels to the right for the next animation
+                timer1.Start();
+            }
         }
     }
+
+
 }
