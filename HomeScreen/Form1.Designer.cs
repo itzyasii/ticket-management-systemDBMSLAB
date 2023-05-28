ac69c2941628926565e87e4c69b2c9203bff54d9
itzyasii <yasirpechuho1@gmail.com>
2023-05-28 21:26:40 +0500

    LATEST UPDATE TO THE CODE

--- HomeScreen/Form1.Designer.cs
+++ HomeScreen/Form1.Designer.cs
@@ -39,34 +39,35 @@
             // HomeButton
             // 
             this.HomeButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeButton.Image")));
-            this.HomeButton.Location = new System.Drawing.Point(541, 11);
-            this.HomeButton.Margin = new System.Windows.Forms.Padding(2);
+            this.HomeButton.Location = new System.Drawing.Point(721, 14);
+            this.HomeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.HomeButton.Name = "HomeButton";
-            this.HomeButton.Size = new System.Drawing.Size(214, 74);
+            this.HomeButton.Size = new System.Drawing.Size(285, 91);
             this.HomeButton.TabIndex = 0;
             this.HomeButton.UseVisualStyleBackColor = true;
             this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(194, 175);
+            this.pictureBox1.Location = new System.Drawing.Point(259, 215);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(965, 388);
+            this.pictureBox1.Size = new System.Drawing.Size(1287, 478);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1384, 801);
+            this.ClientSize = new System.Drawing.Size(1845, 986);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.HomeButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Bookme.pk - Home";
--- HomeScreen/Form1.cs
+++ HomeScreen/Form1.cs
@@ -12,78 +12,82 @@ namespace HomeScreen
 {
     public partial class Form1 : Form
     {
-
-      
-        public Form1()
-        {
-            InitializeComponent();
-            this.Size = new Size(1400, 840);
-
-
-            targetX = pictureBox1.Left + 100; // Example: slide the image 100 pixels to the right
-            stepSize = 5; // Example: move the image 5 pixels in each step
-
-            timer1.Tick += timer1_Tick; // Hook up the event handler for the Timer control
-            timer1.Start(); // Start the animation
-
-        }
-
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            isImageChangePending = true;
-
-        }
-
-        private void HomeButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            timer1.Start();
-
-        }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
-
-        }
         private int currentImageIndex = 0;
         private int targetX;
         private int stepSize = 5;
         private bool isAnimating = false;
 
         private Image[] images = new Image[]
-{
-    Image.FromFile("E:\\Visual Studio Workspace C#\\DBMSLab-project\\ticket-management-systemDBMSLAB\\HomeScreen\\Bus image.jpg"), // Replace with the path to your first image
-    Image.FromFile("E:\\Visual Studio Workspace C#\\DBMSLab-project\\ticket-management-systemDBMSLAB\\HomeScreen\\Busimage2.jpg"), // Replace with the path to your first image
-    Image.FromFile("E:\\Visual Studio Workspace C#\\DBMSLab-project\\ticket-management-systemDBMSLAB\\HomeScreen\\railwayweb.jpg"), // Replace with the path to your first image
-    Image.FromFile("E:\\Visual Studio Workspace C#\\DBMSLab-project\\ticket-management-systemDBMSLAB\\HomeScreen\\rentcarweb.png"), // Replace with the path to your first image
-    // Replace with the path to your second image
-    // Add more images as needed
-};
-    }
-    private void timer1_Tick(object sender, EventArgs e)
-    {
-        if (isAnimating)
         {
-            if (pictureBox1.Left < targetX)
+            Image.FromFile(@"E:\Visual Studio Workspace C#\DBMSLab-project\ticket-management-systemDBMSLAB\HomeScreen\images\Busimage2.jpg"),
+            Image.FromFile(@"E:\Visual Studio Workspace C#\DBMSLab-project\ticket-management-systemDBMSLAB\HomeScreen\images\Busimage.jpg"),
+            Image.FromFile(@"E:\Visual Studio Workspace C#\DBMSLab-project\ticket-management-systemDBMSLAB\HomeScreen\images\railwayweb.jpg"),
+            Image.FromFile(@"E:\Visual Studio Workspace C#\DBMSLab-project\ticket-management-systemDBMSLAB\HomeScreen\images\rentcarweb.png")
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Size = new Size(1400, 840);
+            targetX = pictureBox1.Left + 100; // Example: slide the image 100 pixels to the right
+            stepSize = 5; // Example: move the image 5 pixels in each step
+            timer1.Tick += timer1_Tick; // Hook up the event handler for the Timer control
+            timer1.Start(); // Start the animation
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isAnimating)
             {
-                pictureBox1.Left += stepSize;
+                if (pictureBox1.Left < targetX)
+                {
+                    pictureBox1.Left += stepSize; // Slide the image in slowly
+                }
+                else
+                {
+                    isAnimating = false;
+                    timer1.Stop();
+                    pictureBox1.Image = images[currentImageIndex];
+                    currentImageIndex = (currentImageIndex + 1) % images.Length;
+                    targetX = pictureBox1.Left - pictureBox1.Width; // Slide the image out quickly
+                    stepSize = (int)(pictureBox1.Width / (timer1.Interval / 1000.0) * 2); // Calculate the step size for 2-second animation
+                    timer1.Interval = 3000; // Set the interval to 3000 milliseconds (3 seconds)
+                    timer1.Start();
+                }
             }
             else
             {
-                isAnimating = false;
-                timer1.Stop();
-                pictureBox1.Image = images[currentImageIndex];
-                currentImageIndex = (currentImageIndex + 1) % images.Length;
-                targetX = pictureBox1.Left + 100; // Example: slide the image 100 pixels to the right for the next animation
-                timer1.Start();
+                if (pictureBox1.Left > targetX)
+                {
+                    pictureBox1.Left -= stepSize; // Slide the image out quickly
+                }
+                else
+                {
+                    isAnimating = true;
+                    timer1.Stop();
+                    pictureBox1.Image = null; // Clear the image
+                    targetX = pictureBox1.Left + pictureBox1.Width + 100; // Example: slide the image 100 pixels to the right for the next animation
+                    stepSize = (int)(pictureBox1.Width / (timer1.Interval / 1000.0) * 2); // Calculate the step size for 2-second animation
+                    timer1.Interval = 3000; // Set the interval to 3000 milliseconds (3 seconds)
+                    timer1.Start();
+                }
             }
         }
+
+        private void HomeButton_Click(object sender, EventArgs e)
+        {
+            // Handle HomeButton click event
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = images[currentImageIndex];
+            targetX = pictureBox1.Left + 100; // Example: slide the image 100 pixels to the right for the first animation
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Handle pictureBox1 click event
+        }
     }
-
-
 }
