734a98e8f8d14fc1d3bf6d6de8838262e93efefe
maaz-lab <kmaaz2753@gmail.com>
2023-05-30 21:32:37 +0500

    design changing

--- HomeScreen/SigninForm.Designer.cs
+++ HomeScreen/SigninForm.Designer.cs
@@ -47,9 +47,9 @@
             this.label1.BackColor = System.Drawing.Color.DodgerBlue;
             this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(1124, 278);
+            this.label1.Location = new System.Drawing.Point(671, 170);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.Size = new System.Drawing.Size(133, 30);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username";
             // 
@@ -59,46 +59,51 @@
             this.label2.BackColor = System.Drawing.Color.DodgerBlue;
             this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(1123, 374);
+            this.label2.Location = new System.Drawing.Point(670, 290);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 25);
+            this.label2.Size = new System.Drawing.Size(128, 30);
             this.label2.TabIndex = 1;
             this.label2.Text = "Password";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // user
             // 
-            this.user.Location = new System.Drawing.Point(1123, 321);
+            this.user.Location = new System.Drawing.Point(670, 223);
+            this.user.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.user.Name = "user";
-            this.user.Size = new System.Drawing.Size(174, 22);
+            this.user.Size = new System.Drawing.Size(195, 26);
             this.user.TabIndex = 2;
             this.user.TextChanged += new System.EventHandler(this.username_TextChanged);
             // 
             // pass
             // 
-            this.pass.Location = new System.Drawing.Point(1123, 416);
+            this.pass.Location = new System.Drawing.Point(670, 342);
+            this.pass.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pass.Name = "pass";
-            this.pass.Size = new System.Drawing.Size(174, 22);
+            this.pass.Size = new System.Drawing.Size(195, 26);
             this.pass.TabIndex = 3;
             this.pass.TextChanged += new System.EventHandler(this.password_TextChanged);
             // 
             // HomeButton
             // 
             this.HomeButton.Image = ((System.Drawing.Image)(resources.GetObject("HomeButton.Image")));
-            this.HomeButton.Location = new System.Drawing.Point(487, 29);
+            this.HomeButton.Location = new System.Drawing.Point(294, 11);
             this.HomeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.HomeButton.Name = "HomeButton";
-            this.HomeButton.Size = new System.Drawing.Size(285, 91);
+            this.HomeButton.Size = new System.Drawing.Size(321, 114);
             this.HomeButton.TabIndex = 5;
             this.HomeButton.UseVisualStyleBackColor = true;
             this.HomeButton.Click += new System.EventHandler(this.HomeButton_Click);
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.pictureBox1.Image = global::HomeScreen.Properties.Resources.Busimage;
-            this.pictureBox1.Location = new System.Drawing.Point(47, 236);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 156);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(984, 394);
+            this.pictureBox1.Size = new System.Drawing.Size(643, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
@@ -106,9 +111,9 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1125, 515);
+            this.label3.Location = new System.Drawing.Point(673, 466);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(225, 17);
+            this.label3.Size = new System.Drawing.Size(250, 20);
             this.label3.TabIndex = 6;
             this.label3.Text = "New here? Let\'s Sign you up here!";
             // 
@@ -118,10 +123,10 @@
             this.signup.FlatAppearance.BorderSize = 3;
             this.signup.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Bold);
             this.signup.ForeColor = System.Drawing.Color.White;
-            this.signup.Location = new System.Drawing.Point(1197, 547);
+            this.signup.Location = new System.Drawing.Point(754, 506);
             this.signup.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.signup.Name = "signup";
-            this.signup.Size = new System.Drawing.Size(140, 36);
+            this.signup.Size = new System.Drawing.Size(158, 45);
             this.signup.TabIndex = 7;
             this.signup.Text = "SIGN UP";
             this.signup.UseVisualStyleBackColor = false;
@@ -134,10 +139,10 @@
             this.signin.FlatAppearance.BorderSize = 3;
             this.signin.Font = new System.Drawing.Font("Mongolian Baiti", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.signin.ForeColor = System.Drawing.Color.White;
-            this.signin.Location = new System.Drawing.Point(1211, 465);
+            this.signin.Location = new System.Drawing.Point(769, 403);
             this.signin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.signin.Name = "signin";
-            this.signin.Size = new System.Drawing.Size(126, 34);
+            this.signin.Size = new System.Drawing.Size(142, 42);
             this.signin.TabIndex = 8;
             this.signin.Text = "SIGN IN";
             this.signin.UseVisualStyleBackColor = false;
@@ -145,10 +150,10 @@
             // 
             // SigninForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1382, 793);
+            this.ClientSize = new System.Drawing.Size(1046, 643);
             this.Controls.Add(this.signin);
             this.Controls.Add(this.signup);
             this.Controls.Add(this.label3);
@@ -159,6 +164,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "SigninForm";
             this.Text = "Signing in";
             this.Load += new System.EventHandler(this.SigninForm_Load);
