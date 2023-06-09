7d75ebeb2d46c58bcbfc7624d88e51b7de74a830
maaz-lab <kmaaz2753@gmail.com>
2023-06-10 01:50:34 +0500

    movie and flight changes

--- HomeScreen/Train.Designer.cs
+++ HomeScreen/Train.Designer.cs
@@ -48,21 +48,22 @@
             this.panel1.Controls.Add(this.comboBox2);
             this.panel1.Controls.Add(this.comboBox1);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(92, 299);
+            this.panel1.Location = new System.Drawing.Point(104, 374);
             this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(434, 275);
+            this.panel1.Size = new System.Drawing.Size(488, 344);
             this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button1.Location = new System.Drawing.Point(19, 200);
+            this.button1.Location = new System.Drawing.Point(21, 250);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(398, 42);
+            this.button1.Size = new System.Drawing.Size(448, 52);
             this.button1.TabIndex = 4;
             this.button1.Text = "BOOK";
             this.button1.UseVisualStyleBackColor = false;
@@ -72,10 +73,10 @@
             // 
             this.dateTimePicker1.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(19, 138);
+            this.dateTimePicker1.Location = new System.Drawing.Point(21, 172);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(399, 30);
+            this.dateTimePicker1.Size = new System.Drawing.Size(448, 34);
             this.dateTimePicker1.TabIndex = 3;
             // 
             // comboBox2
@@ -83,10 +84,10 @@
             this.comboBox2.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox2.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(220, 82);
+            this.comboBox2.Location = new System.Drawing.Point(248, 102);
             this.comboBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(197, 29);
+            this.comboBox2.Size = new System.Drawing.Size(221, 31);
             this.comboBox2.TabIndex = 2;
             this.comboBox2.Text = "To";
             // 
@@ -95,10 +96,10 @@
             this.comboBox1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.ForeColor = System.Drawing.SystemColors.WindowFrame;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(19, 82);
+            this.comboBox1.Location = new System.Drawing.Point(21, 102);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(197, 29);
+            this.comboBox1.Size = new System.Drawing.Size(221, 31);
             this.comboBox1.TabIndex = 1;
             this.comboBox1.Text = "From";
             // 
@@ -107,19 +108,19 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label1.Location = new System.Drawing.Point(44, 34);
+            this.label1.Location = new System.Drawing.Point(50, 42);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(358, 21);
+            this.label1.Size = new System.Drawing.Size(424, 23);
             this.label1.TabIndex = 0;
             this.label1.Text = "We\'re bringing you a new level of comfort.";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-1, -149);
+            this.pictureBox1.Location = new System.Drawing.Point(-1, -186);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1268, 976);
+            this.pictureBox1.Size = new System.Drawing.Size(1426, 1220);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -127,10 +128,10 @@
             // 
             // Train
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.ClientSize = new System.Drawing.Size(1420, 841);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
